3d25514247f4799faa2ea62f2e5a57e431b22a2f
atalavera2019 <43644746+atalavera2019@users.noreply.github.com>
2019-01-08 21:42:22 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".toggle").click(function(){
+ $("#marble").slideToggle();
+});
+
+$(".fade").click(function(){
+ $("#card").fadeToggle();
+});
+
+$(".hide").click(function(){
+ $("#rabbit").toggle();
+});
+
+
+
